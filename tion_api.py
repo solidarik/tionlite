ac01d2c35fabb7bf0db0d693556ae1ebad5b35ad
Ildar Gubaydullin <ildar.gubaydullin@gmail.com>
2025-02-12 08:50:42 +0500

    feat(repeat): Add repeat n times

--- tion_api.py
+++ tion_api.py
@@ -1,4 +1,31 @@
+from functools import wraps
+from typing import Awaitable, Callable, TypeVar
+
 from tion_btle import TionLite
+
+T = TypeVar("T")
+
+
+def repeat_n_times(n: int):
+    """
+    Decorator factory to repeat an async function n times.
+    """
+
+    def decorator(
+        async_func: Callable[..., Awaitable[T]]
+    ) -> Callable[..., Awaitable[T]]:
+        @wraps(async_func)
+        async def wrapper(*args, **kwargs) -> T:
+            results = []
+            for i in range(n):
+                result = await async_func(*args, **kwargs)
+                results.append(result)
+                print(f"Call {i + 1} result: {result}")
+            return results[-1]
+
+        return wrapper
+
+    return decorator
 
 
 class TionApi:
@@ -20,6 +47,7 @@ class TionApi:
     async def switch_off(self):
         await self.set({"state": "off"})
 
+    @repeat_n_times(3)
     async def change_params(self, *, fan_speed: int, heater_temp: int = 12):
         await self.device.set(
             {
